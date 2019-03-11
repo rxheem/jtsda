76cd82464366cf9110326c8786a3313408a1cdd9
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-03-11 09:16:51 -0500

    Updates

--- public/assets/js/master.js
+++ public/assets/js/master.js
@@ -1,8 +1,17 @@
 'use strict'
+console.clear()
 
 /* Requesting permission from the user to allow notifications early on
 ** We may eventually start offering a notification service where we alerts
 ** users on events and postings
 */
 if (Notification.permission !== "granted")
-  Notification.requestPermission();
+  Notification.requestPermission()
+
+// using es modules
+import device from 'current-device'
+
+// common.js
+const device = require('current-device').default
+
+console.log('Testing')
