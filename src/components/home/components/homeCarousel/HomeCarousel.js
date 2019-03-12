1a0f05e46fd60fbaf65806fe1f25a1df5ef058c1
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-03-11 22:10:16 -0500

    Remove item

--- src/components/home/components/homeCarousel/HomeCarousel.js
+++ src/components/home/components/homeCarousel/HomeCarousel.js
@@ -10,17 +10,7 @@ class HomeCarousel extends Component {
   render () {
     return (
       <Carousel>
-        <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src='https://www.theedigital.com/wp-content/uploads/2017/05/marketing-cat-memes-003.png'
-          alt='hello'
-        />
-        <Carousel.Caption>
-          <h3>Hi</h3>
-          <p>Mhmmm</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+        
       </Carousel>
     )
   }
